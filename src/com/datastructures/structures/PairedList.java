2734db4fa5477bc10a18eb5e8d095754ae28e414
bhargav <bhargavreddypingle@gmail.com>
2022-01-09 14:27:00 -0600

    paired list logic added

--- src/com/datastructures/structures/PairedList.java
+++ src/com/datastructures/structures/PairedList.java
@@ -1,4 +1,38 @@
 package com.datastructures.structures;
 
-public class PairedList {
+import java.util.ArrayList;
+import java.util.List;
+
+public class PairedList<A, B> {
+    private static class Pair<A, B> {
+        A first;
+        B second;
+
+        Pair(A first, B second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+
+    private final List<Pair<A, B>> pairs = new ArrayList<>();
+
+    public void add(A a, B b) {
+        pairs.add(new Pair<>(a, b));
+    }
+
+    public List<A> getFirsts() {
+        List<A> list = new ArrayList<>();
+        for (Pair<A, B> pair : pairs) list.add(pair.first);
+        return list;
+    }
+
+    public List<B> getSeconds() {
+        List<B> list = new ArrayList<>();
+        for (Pair<A, B> pair : pairs) list.add(pair.second);
+        return list;
+    }
+
+    public int size() {
+        return pairs.size();
+    }
 }
