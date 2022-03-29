0350a7b01cda17f4ca0df8ec2616fbb136c49a37
bhargav <bhargavreddypingle@gmail.com>
2022-03-29 14:27:00 -0500

    null skipping list logic added

--- src/com/datastructures/structures/NullSkippingList.java
+++ src/com/datastructures/structures/NullSkippingList.java
@@ -1,4 +1,26 @@
 package com.datastructures.structures;
 
-public class NullSkippingList {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class NullSkippingList<T> implements Iterable<T> {
+    private final List<T> list = new ArrayList<>();
+
+    public void add(T item) {
+        list.add(item);
+    }
+
+    public List<T> getNonNulls() {
+        List<T> nonNulls = new ArrayList<>();
+        for (T item : list) {
+            if (item != null) nonNulls.add(item);
+        }
+        return nonNulls;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return getNonNulls().iterator();
+    }
 }
