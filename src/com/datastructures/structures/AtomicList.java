a372af4a6959b2608517bf8e093921c117fc4fc6
bhargav <bhargavreddypingle@gmail.com>
2022-06-09 14:27:00 -0500

    atomic list logic added

--- src/com/datastructures/structures/AtomicList.java
+++ src/com/datastructures/structures/AtomicList.java
@@ -1,4 +1,25 @@
 package com.datastructures.structures;
 
-public class AtomicList {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class AtomicList<T> {
+    private final CopyOnWriteArrayList<T> list = new CopyOnWriteArrayList<>();
+
+    public void add(T item) {
+        list.add(item);
+    }
+
+    public T get(int index) {
+        return list.get(index);
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public List<T> snapshot() {
+        return List.copyOf(list);
+    }
 }
+
