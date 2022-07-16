3d1ee5f9bba74293e9f8471fb247ab9820ba2b97
bhargav <bhargavreddypingle@gmail.com>
2022-07-16 14:27:00 -0500

    sharded list added logic

--- src/com/datastructures/structures/ShardedList.java
+++ src/com/datastructures/structures/ShardedList.java
@@ -1,4 +1,33 @@
 package com.datastructures.structures;
 
-public class ShardedList {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShardedList<T> {
+    private final List<List<T>> shards = new ArrayList<>();
+    private final int shardSize;
+
+    public ShardedList(int shardSize) {
+        this.shardSize = shardSize;
+        shards.add(new ArrayList<>());
+    }
+
+    public void add(T item) {
+        List<T> last = shards.get(shards.size() - 1);
+        if (last.size() >= shardSize) {
+            last = new ArrayList<>();
+            shards.add(last);
+        }
+        last.add(item);
+    }
+
+    public T get(int index) {
+        int shardIndex = index / shardSize;
+        int localIndex = index % shardSize;
+        return shards.get(shardIndex).get(localIndex);
+    }
+
+    public int size() {
+        return shards.stream().mapToInt(List::size).sum();
+    }
 }
