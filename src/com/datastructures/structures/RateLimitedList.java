97cc087593d639f05343aaa48964d8c0873c5687
bhargav <bhargavreddypingle@gmail.com>
2022-11-13 14:27:00 -0600

    rate limited list logic added

--- src/com/datastructures/structures/RateLimitedList.java
+++ src/com/datastructures/structures/RateLimitedList.java
@@ -1,4 +1,28 @@
 package com.datastructures.structures;
 
-public class RateLimitedList {
+import java.util.ArrayList;
+import java.util.List;
+
+public class RateLimitedList<T> {
+    private final List<T> list = new ArrayList<>();
+    private final long delayMillis;
+    private long lastAddedTime = 0;
+
+    public RateLimitedList(long delayMillis) {
+        this.delayMillis = delayMillis;
+    }
+
+    public boolean add(T item) {
+        long now = System.currentTimeMillis();
+        if (now - lastAddedTime >= delayMillis) {
+            list.add(item);
+            lastAddedTime = now;
+            return true;
+        }
+        return false;
+    }
+
+    public List<T> getAll() {
+        return new ArrayList<>(list);
+    }
 }
