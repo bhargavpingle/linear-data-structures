fc066ce427573fc034c96ce3a745798a9b3d4cb2
bhargav <bhargavreddypingle@gmail.com>
2023-05-14 14:27:00 -0500

    event list logic added

--- src/com/datastructures/structures/EventList.java
+++ src/com/datastructures/structures/EventList.java
@@ -1,4 +1,33 @@
 package com.datastructures.structures;
 
-public class EventList {
+import java.time.Instant;
+import java.util.LinkedList;
+
+public class EventList<T> {
+    private static class Event<T> {
+        T value;
+        Instant timestamp;
+
+        Event(T value) {
+            this.value = value;
+            this.timestamp = Instant.now();
+        }
+    }
+
+    private final LinkedList<Event<T>> events = new LinkedList<>();
+
+    public void record(T value) {
+        events.add(new Event<>(value));
+    }
+
+    public LinkedList<T> getRecent(long seconds) {
+        LinkedList<T> result = new LinkedList<>();
+        Instant cutoff = Instant.now().minusSeconds(seconds);
+        for (Event<T> e : events) {
+            if (e.timestamp.isAfter(cutoff)) {
+                result.add(e.value);
+            }
+        }
+        return result;
+    }
 }
