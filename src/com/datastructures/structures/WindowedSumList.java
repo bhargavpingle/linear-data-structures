3af1481c46f1563d52f8d34410e5a1926e45b183
bhargav <bhargavreddypingle@gmail.com>
2023-05-24 14:27:00 -0500

    windowed sum list logic added

--- src/com/datastructures/structures/WindowedSumList.java
+++ src/com/datastructures/structures/WindowedSumList.java
@@ -1,4 +1,29 @@
 package com.datastructures.structures;
 
+import java.util.LinkedList;
+
 public class WindowedSumList {
+    private final int windowSize;
+    private final LinkedList<Integer> window = new LinkedList<>();
+    private int sum = 0;
+
+    public WindowedSumList(int windowSize) {
+        this.windowSize = windowSize;
+    }
+
+    public void add(int value) {
+        window.add(value);
+        sum += value;
+        if (window.size() > windowSize) {
+            sum -= window.removeFirst();
+        }
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public double getAverage() {
+        return window.isEmpty() ? 0 : (double) sum / window.size();
+    }
 }
