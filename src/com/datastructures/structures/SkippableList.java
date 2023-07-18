7c99d15d6f7c1c1dce7349620c4b90da8d9fd985
bhargav <bhargavreddypingle@gmail.com>
2023-07-18 14:27:00 -0500

    skippable list logic added

--- src/com/datastructures/structures/SkippableList.java
+++ src/com/datastructures/structures/SkippableList.java
@@ -1,4 +1,23 @@
 package com.datastructures.structures;
 
-public class SkippableList {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SkippableList<T> {
+    private final List<T> list = new ArrayList<>();
+
+    public void add(T item) {
+        list.add(item);
+    }
+
+    public List<T> filter(Predicate<T> predicate) {
+        List<T> result = new ArrayList<>();
+        for (T item : list) {
+            if (predicate.test(item)) {
+                result.add(item);
+            }
+        }
+        return result;
+    }
 }
