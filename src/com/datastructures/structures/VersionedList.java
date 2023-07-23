60eb5b704635d11c2cc7d0bcf04543d8b2a14e6a
bhargav <bhargavreddypingle@gmail.com>
2023-07-23 14:27:00 -0500

    staggered list intialized

--- src/com/datastructures/structures/VersionedList.java
+++ src/com/datastructures/structures/VersionedList.java
@@ -1,4 +1,38 @@
 package com.datastructures.structures;
 
-public class VersionedList {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class VersionedList<T> {
+    private final Map<Integer, List<T>> versions = new HashMap<>();
+    private List<T> current = new ArrayList<>();
+    private int version = 0;
+
+    public void add(T value) {
+        snapshot();
+        current.add(value);
+    }
+
+    public void remove(int index) {
+        snapshot();
+        current.remove(index);
+    }
+
+    private void snapshot() {
+        versions.put(++version, new ArrayList<>(current));
+    }
+
+    public List<T> getVersion(int ver) {
+        return versions.getOrDefault(ver, new ArrayList<>());
+    }
+
+    public List<T> getCurrent() {
+        return new ArrayList<>(current);
+    }
+
+    public int getCurrentVersion() {
+        return version;
+    }
 }
