d23071de40bf1fc554f7545fc43acf8a8cdbc1a1
bhargav <bhargavreddypingle@gmail.com>
2023-07-26 14:27:00 -0500

    transactional list logic added

--- src/com/datastructures/structures/TransactionalList.java
+++ src/com/datastructures/structures/TransactionalList.java
@@ -1,4 +1,34 @@
 package com.datastructures.structures;
 
-public class TransactionalList {
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionalList<T> {
+    private final List<T> committed = new ArrayList<>();
+    private List<T> uncommitted = null;
+
+    public void begin() {
+        uncommitted = new ArrayList<>(committed);
+    }
+
+    public void add(T value) {
+        if (uncommitted == null) throw new IllegalStateException("Transaction not started");
+        uncommitted.add(value);
+    }
+
+    public void commit() {
+        if (uncommitted != null) {
+            committed.clear();
+            committed.addAll(uncommitted);
+            uncommitted = null;
+        }
+    }
+
+    public void rollback() {
+        uncommitted = null;
+    }
+
+    public List<T> getState() {
+        return new ArrayList<>(uncommitted != null ? uncommitted : committed);
+    }
 }
