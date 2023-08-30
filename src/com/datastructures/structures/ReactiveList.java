74094c71bbe8a66e78dc0c770c5dbc134f175014
bhargav <bhargavreddypingle@gmail.com>
2023-08-30 14:27:00 -0500

    skippable list logic added

--- src/com/datastructures/structures/ReactiveList.java
+++ src/com/datastructures/structures/ReactiveList.java
@@ -1,4 +1,29 @@
 package com.datastructures.structures;
 
-public class ReactiveList {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ReactiveList<T> {
+    private final List<T> list = new ArrayList<>();
+    private final List<Consumer<T>> listeners = new ArrayList<>();
+
+    public void addListener(Consumer<T> listener) {
+        listeners.add(listener);
+    }
+
+    public void add(T item) {
+        list.add(item);
+        for (Consumer<T> listener : listeners) {
+            listener.accept(item);
+        }
+    }
+
+    public T get(int index) {
+        return list.get(index);
+    }
+
+    public int size() {
+        return list.size();
+    }
 }
