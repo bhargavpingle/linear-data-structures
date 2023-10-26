eae1f3f0beade5913132504bc57b61e87703654d
bhargav <bhargavreddypingle@gmail.com>
2023-10-26 14:02:00 -0500

    stack logic added

--- src/com/datastructures/structures/Stack.java
+++ src/com/datastructures/structures/Stack.java
@@ -1,4 +1,35 @@
 package com.datastructures.structures;
 
-public class Stack {
+import java.util.EmptyStackException;
+
+public class Stack<T> {
+    private Node<T> top;
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+        Node(T data) { this.data = data; }
+    }
+
+    public void push(T data) {
+        Node<T> node = new Node<>(data);
+        node.next = top;
+        top = node;
+    }
+
+    public T pop() {
+        if (top == null) throw new EmptyStackException();
+        T data = top.data;
+        top = top.next;
+        return data;
+    }
+
+    public T peek() {
+        if (top == null) throw new EmptyStackException();
+        return top.data;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
 }
