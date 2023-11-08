f0f99fbcde8ad56a403ca85f6278da55caccc2fd
bhargav <bhargavreddypingle@gmail.com>
2023-11-08 14:02:00 -0600

    stack test cases added

--- src/com/datastructures/tests/StackTest.java
+++ src/com/datastructures/tests/StackTest.java
@@ -1,4 +1,34 @@
 package com.datastructures.tests;
 
+import com.datastructures.structures.Stack;
+import org.junit.jupiter.api.Test;
+
+import java.util.EmptyStackException;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class StackTest {
+
+    @Test
+    public void testPushPopPeek() {
+        Stack<Integer> stack = new Stack<>();
+        assertTrue(stack.isEmpty());
+
+        stack.push(10);
+        stack.push(20);
+        assertFalse(stack.isEmpty());
+        assertEquals(20, stack.peek());
+
+        assertEquals(20, stack.pop());
+        assertEquals(10, stack.peek());
+        assertEquals(10, stack.pop());
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    public void testPopEmptyStack() {
+        Stack<Integer> stack = new Stack<>();
+        assertThrows(EmptyStackException.class, stack::pop);
+    }
 }
