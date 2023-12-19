ca2d3ae1fe0bf460048ff0bebab53987a03d3553
bhargav <bhargavreddypingle@gmail.com>
2023-12-19 14:07:00 -0600

    array list custom initialized

--- src/com/datastructures/structures/ArrayListCustom.java
+++ src/com/datastructures/structures/ArrayListCustom.java
@@ -1,4 +1,48 @@
 package com.datastructures.structures;
 
-public class ArrayListCustom {
+import java.util.Arrays;
+
+public class ArrayListCustom<T> {
+    private Object[] array;
+    private int size;
+
+    public ArrayListCustom() {
+        array = new Object[10];
+        size = 0;
+    }
+
+    public void add(T value) {
+        ensureCapacity();
+        array[size++] = value;
+    }
+
+    public T get(int index) {
+        checkIndex(index);
+        return (T) array[index];
+    }
+
+    public T remove(int index) {
+        checkIndex(index);
+        T removed = (T) array[index];
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        array[--size] = null;
+        return removed;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void ensureCapacity() {
+        if (size == array.length) {
+            array = Arrays.copyOf(array, array.length * 2);
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index >= size || index < 0)
+            throw new IndexOutOfBoundsException();
+    }
 }
