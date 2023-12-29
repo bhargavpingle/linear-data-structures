4740d8d027b96e0b5b1a78667af56b47553bb677
bhargav <bhargavreddypingle@gmail.com>
2023-12-29 14:27:00 -0600

    staggered list logic added

--- src/com/datastructures/structures/StaggeredList.java
+++ src/com/datastructures/structures/StaggeredList.java
@@ -1,4 +1,28 @@
 package com.datastructures.structures;
 
-public class StaggeredList {
+import java.util.ArrayList;
+import java.util.List;
+
+public class StaggeredList<T> {
+    private final List<T> fastTier = new ArrayList<>();
+    private final List<T> slowTier = new ArrayList<>();
+    private final int threshold;
+
+    public StaggeredList(int threshold) {
+        this.threshold = threshold;
+    }
+
+    public void add(T item) {
+        if (fastTier.size() >= threshold) {
+            slowTier.addAll(fastTier);
+            fastTier.clear();
+        }
+        fastTier.add(item);
+    }
+
+    public List<T> getAll() {
+        List<T> combined = new ArrayList<>(slowTier);
+        combined.addAll(fastTier);
+        return combined;
+    }
 }
