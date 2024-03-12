30e0486bfaad9bc07c1d47a9d0c9642ea9afbfe6
bhargav <bhargavreddypingle@gmail.com>
2024-03-12 14:07:00 -0500

    doubly linked list logic added

--- src/com/datastructures/structures/DoublyLinkedList.java
+++ src/com/datastructures/structures/DoublyLinkedList.java
@@ -1,4 +1,61 @@
 package com.datastructures.structures;
 
-public class DoublyLinkedList {
+public class DoublyLinkedList<T> {
+    private class Node {
+        T data;
+        Node prev, next;
+
+        Node(T data) {
+            this.data = data;
+        }
+    }
+
+    private Node head, tail;
+    private int size;
+
+    public void addFirst(T data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void addLast(T data) {
+        Node newNode = new Node(data);
+        if (tail == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public void printForward() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public void printBackward() {
+        Node current = tail;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.prev;
+        }
+        System.out.println();
+    }
+
+    public int size() {
+        return size;
+    }
 }
