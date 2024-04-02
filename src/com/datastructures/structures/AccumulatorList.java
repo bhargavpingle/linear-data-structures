288bd9fedc364f78a6af23b31c117b7d992f905c
bhargav <bhargavreddypingle@gmail.com>
2024-04-02 14:27:00 -0500

    accumulator list logic added

--- src/com/datastructures/structures/AccumulatorList.java
+++ src/com/datastructures/structures/AccumulatorList.java
@@ -1,4 +1,23 @@
 package com.datastructures.structures;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AccumulatorList {
+    private final List<Integer> list = new ArrayList<>();
+    private int sum = 0;
+
+    public void add(int val) {
+        sum += val;
+        list.add(sum);
+    }
+
+    public int getSumUpTo(int index) {
+        if (index < 0 || index >= list.size()) return 0;
+        return list.get(index);
+    }
+
+    public List<Integer> getAccumulated() {
+        return list;
+    }
 }
