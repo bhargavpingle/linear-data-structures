4f27613a904378b69b78d84df9617e49a182b3df
bhargav <bhargavreddypingle@gmail.com>
2024-06-08 14:27:00 -0500

    self shuffling list logic added

--- src/com/datastructures/structures/SelfShufflingList.java
+++ src/com/datastructures/structures/SelfShufflingList.java
@@ -1,4 +1,32 @@
 package com.datastructures.structures;
 
-public class SelfShufflingList {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SelfShufflingList<T> {
+    private final List<T> data = new ArrayList<>();
+    private final int shuffleInterval;
+    private int accessCount = 0;
+
+    public SelfShufflingList(int shuffleInterval) {
+        this.shuffleInterval = shuffleInterval;
+    }
+
+    public void add(T item) {
+        data.add(item);
+    }
+
+    public T get(int index) {
+        accessCount++;
+        if (accessCount >= shuffleInterval) {
+            Collections.shuffle(data);
+            accessCount = 0;
+        }
+        return data.get(index);
+    }
+
+    public List<T> getAll() {
+        return data;
+    }
 }
