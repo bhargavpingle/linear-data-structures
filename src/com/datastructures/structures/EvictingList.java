463c5ecd2b3a5bf3cadcdc33ac5c99e38c5c2c23
bhargav <bhargavreddypingle@gmail.com>
2024-06-19 14:27:00 -0500

    cicular list iterator logic added

--- src/com/datastructures/structures/EvictingList.java
+++ src/com/datastructures/structures/EvictingList.java
@@ -1,4 +1,31 @@
 package com.datastructures.structures;
 
-public class EvictingList {
+import java.util.LinkedList;
+
+public class EvictingList<T> {
+    private final LinkedList<T> list = new LinkedList<>();
+    private final int maxSize;
+
+    public EvictingList(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public void add(T item) {
+        if (list.size() == maxSize) {
+            list.removeFirst();
+        }
+        list.addLast(item);
+    }
+
+    public T get(int index) {
+        return list.get(index);
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public LinkedList<T> getAll() {
+        return new LinkedList<>(list);
+    }
 }
