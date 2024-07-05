c4d8bb02887dce00620d587c7c007fca1e61b8bf
bhargav <bhargavreddypingle@gmail.com>
2024-07-05 14:07:00 -0500

    rope logic added

--- src/com/datastructures/structures/Rope.java
+++ src/com/datastructures/structures/Rope.java
@@ -1,4 +1,35 @@
 package com.datastructures.structures;
 
 public class Rope {
+    private class Node {
+        String data;
+        Node next;
+        Node(String data) { this.data = data; }
+    }
+
+    private Node head;
+
+    public void append(String str) {
+        Node newNode = new Node(str);
+        if (head == null) head = newNode;
+        else {
+            Node curr = head;
+            while (curr.next != null) curr = curr.next;
+            curr.next = newNode;
+        }
+    }
+
+    public String build() {
+        StringBuilder sb = new StringBuilder();
+        Node curr = head;
+        while (curr != null) {
+            sb.append(curr.data);
+            curr = curr.next;
+        }
+        return sb.toString();
+    }
+
+    public void print() {
+        System.out.println(build());
+    }
 }
