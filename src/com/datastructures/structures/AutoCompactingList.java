eabf042b95df867b4d1ee4c2b3d925190c1bcf81
bhargav <bhargavreddypingle@gmail.com>
2024-08-05 14:27:00 -0500

    auto compacting list logic added

--- src/com/datastructures/structures/AutoCompactingList.java
+++ src/com/datastructures/structures/AutoCompactingList.java
@@ -1,4 +1,30 @@
 package com.datastructures.structures;
 
-public class AutoCompactingList {
+import java.util.LinkedList;
+
+public class AutoCompactingList<T> {
+    private final LinkedList<T> list = new LinkedList<>();
+    private final int maxSize;
+
+    public AutoCompactingList(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public void add(T item) {
+        list.add(item);
+        if (list.size() > maxSize) {
+            compact();
+        }
+    }
+
+    private void compact() {
+        // Remove every 2nd element to compact
+        for (int i = 1; i < list.size(); i++) {
+            list.remove(i);
+        }
+    }
+
+    public LinkedList<T> getAll() {
+        return list;
+    }
 }
