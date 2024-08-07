6fb1f121040449b091195b284fecc381bf48743a
bhargav <bhargavreddypingle@gmail.com>
2024-08-07 14:07:00 -0500

    Bucket List logic added

--- src/com/datastructures/structures/BucketList.java
+++ src/com/datastructures/structures/BucketList.java
@@ -1,4 +1,27 @@
 package com.datastructures.structures;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BucketList {
+    private List<List<Integer>> buckets;
+
+    public BucketList(int numBuckets) {
+        buckets = new ArrayList<>();
+        for (int i = 0; i < numBuckets; i++) {
+            buckets.add(new ArrayList<>());
+        }
+    }
+
+    public void add(int value) {
+        int bucketIndex = value % buckets.size();
+        buckets.get(bucketIndex).add(value);
+    }
+
+    public void print() {
+        for (int i = 0; i < buckets.size(); i++) {
+            System.out.println("Bucket " + i + ": " + buckets.get(i));
+        }
+    }
 }
+
