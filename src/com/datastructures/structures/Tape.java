360756cd8f507ae71854166da0d34d9182111423
bhargav <bhargavreddypingle@gmail.com>
2024-08-30 14:07:00 -0500

    tape logic added

--- src/com/datastructures/structures/Tape.java
+++ src/com/datastructures/structures/Tape.java
@@ -1,4 +1,35 @@
 package com.datastructures.structures;
 
+import java.util.HashMap;
+
 public class Tape {
+    private HashMap<Integer, Character> tape = new HashMap<>();
+    private int head = 0;
+
+    public void write(char c) {
+        tape.put(head, c);
+    }
+
+    public char read() {
+        return tape.getOrDefault(head, '_');
+    }
+
+    public void moveLeft() {
+        head--;
+    }
+
+    public void moveRight() {
+        head++;
+    }
+
+    public int getPosition() {
+        return head;
+    }
+
+    public void printTape() {
+        for (int i = head - 5; i <= head + 5; i++) {
+            System.out.print(tape.getOrDefault(i, '_') + " ");
+        }
+        System.out.println();
+    }
 }
