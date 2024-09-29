525dd92908c5a9e1107b21501f275ad1929c3462
bhargav <bhargavreddypingle@gmail.com>
2024-09-29 14:07:00 -0500

    Bit array logic added

--- src/com/datastructures/structures/BitArray.java
+++ src/com/datastructures/structures/BitArray.java
@@ -1,4 +1,30 @@
 package com.datastructures.structures;
 
 public class BitArray {
+    private byte[] bits;
+    private int size;
+
+    public BitArray(int numBits) {
+        size = numBits;
+        bits = new byte[(numBits + 7) / 8];
+    }
+
+    public void set(int index) {
+        bits[index / 8] |= (1 << (index % 8));
+    }
+
+    public void clear(int index) {
+        bits[index / 8] &= ~(1 << (index % 8));
+    }
+
+    public boolean get(int index) {
+        return (bits[index / 8] & (1 << (index % 8))) != 0;
+    }
+
+    public void printBits() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(get(i) ? "1" : "0");
+        }
+        System.out.println();
+    }
 }
