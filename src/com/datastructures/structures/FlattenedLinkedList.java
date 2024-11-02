2d7f841ec7993b894a0d7160a9c606b3fdf0c4d5
bhargav <bhargavreddypingle@gmail.com>
2024-11-02 14:07:00 -0500

    Flattened linked logic added

--- src/com/datastructures/structures/FlattenedLinkedList.java
+++ src/com/datastructures/structures/FlattenedLinkedList.java
@@ -1,4 +1,33 @@
 package com.datastructures.structures;
 
-public class FlattenedLinkedList {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class FlattenedLinkedList<T> implements Iterable<T> {
+    private List<List<T>> data = new ArrayList<>();
+
+    public void addList(List<T> subList) {
+        data.add(subList);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            int listIndex = 0, itemIndex = 0;
+
+            public boolean hasNext() {
+                while (listIndex < data.size()) {
+                    if (itemIndex < data.get(listIndex).size()) return true;
+                    listIndex++;
+                    itemIndex = 0;
+                }
+                return false;
+            }
+
+            public T next() {
+                return data.get(listIndex).get(itemIndex++);
+            }
+        };
+    }
 }
