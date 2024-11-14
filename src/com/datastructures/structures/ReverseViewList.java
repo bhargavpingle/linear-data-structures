c8baf4149182578037872d21496ba6db7e68447a
bhargav <bhargavreddypingle@gmail.com>
2024-11-14 14:27:00 -0600

    Reverse view list logic added

--- src/com/datastructures/structures/ReverseViewList.java
+++ src/com/datastructures/structures/ReverseViewList.java
@@ -1,4 +1,19 @@
 package com.datastructures.structures;
 
-public class ReverseViewList {
+import java.util.List;
+
+public class ReverseViewList<T> {
+    private final List<T> base;
+
+    public ReverseViewList(List<T> base) {
+        this.base = base;
+    }
+
+    public T get(int index) {
+        return base.get(base.size() - 1 - index);
+    }
+
+    public int size() {
+        return base.size();
+    }
 }
