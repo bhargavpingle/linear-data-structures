d4327bf7fb61ec25d4c5b5f53c218b6fdb20f572
bhargav <bhargavreddypingle@gmail.com>
2024-11-30 14:07:00 -0600

    Free List logic added

--- src/com/datastructures/structures/FreeList.java
+++ src/com/datastructures/structures/FreeList.java
@@ -1,4 +1,19 @@
 package com.datastructures.structures;
 
-public class FreeList {
+import java.util.Stack;
+
+public class FreeList<T> {
+    private final Stack<T> pool = new Stack<>();
+
+    public void release(T item) {
+        pool.push(item);
+    }
+
+    public T acquire() {
+        return pool.isEmpty() ? null : pool.pop();
+    }
+
+    public int available() {
+        return pool.size();
+    }
 }
