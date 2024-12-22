4a59d612baa00f426ed32f8a9d6b9b7b8613f062
bhargav <bhargavreddypingle@gmail.com>
2024-12-22 14:07:00 -0600

    sliding window logic added

--- src/com/datastructures/structures/SlidingWindowQueue.java
+++ src/com/datastructures/structures/SlidingWindowQueue.java
@@ -1,4 +1,32 @@
 package com.datastructures.structures;
 
-public class SlidingWindowQueue {
+import java.util.LinkedList;
+
+public class SlidingWindowQueue<T> {
+    private final LinkedList<T> window = new LinkedList<>();
+    private final int capacity;
+
+    public SlidingWindowQueue(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void add(T value) {
+        if (window.size() == capacity) {
+            window.removeFirst();
+        }
+        window.addLast(value);
+    }
+
+    public T oldest() {
+        return window.getFirst();
+    }
+
+    public T newest() {
+        return window.getLast();
+    }
+
+    public void print() {
+        System.out.println(window);
+    }
 }
+
