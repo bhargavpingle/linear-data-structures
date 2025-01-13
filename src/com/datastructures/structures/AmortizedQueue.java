c68f7232f1295efa40a2992fbc29a315bf83b923
bhargav <bhargavreddypingle@gmail.com>
2025-01-13 14:07:00 -0600

    amortized queue logic added

--- src/com/datastructures/structures/AmortizedQueue.java
+++ src/com/datastructures/structures/AmortizedQueue.java
@@ -1,4 +1,34 @@
 package com.datastructures.structures;
 
-public class AmortizedQueue {
+import java.util.Stack;
+
+public class AmortizedQueue<T> {
+    private Stack<T> stackNewest = new Stack<>();
+    private Stack<T> stackOldest = new Stack<>();
+
+    public void enqueue(T value) {
+        stackNewest.push(value);
+    }
+
+    public T dequeue() {
+        shiftStacks();
+        return stackOldest.pop();
+    }
+
+    public T peek() {
+        shiftStacks();
+        return stackOldest.peek();
+    }
+
+    public boolean isEmpty() {
+        return stackNewest.isEmpty() && stackOldest.isEmpty();
+    }
+
+    private void shiftStacks() {
+        if (stackOldest.isEmpty()) {
+            while (!stackNewest.isEmpty()) {
+                stackOldest.push(stackNewest.pop());
+            }
+        }
+    }
 }
