19636ce0be04db5f83db3df084ec4b570bb13e93
bhargav <bhargavreddypingle@gmail.com>
2025-01-25 14:07:00 -0600

    Flat Map list logic added

--- src/com/datastructures/structures/FlatMapList.java
+++ src/com/datastructures/structures/FlatMapList.java
@@ -1,4 +1,21 @@
 package com.datastructures.structures;
 
-public class FlatMapList {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class FlatMapList<T, R> {
+    private final List<T> input;
+
+    public FlatMapList(List<T> input) {
+        this.input = input;
+    }
+
+    public List<R> flatMap(Function<T, List<R>> mapper) {
+        List<R> result = new ArrayList<>();
+        for (T t : input) {
+            result.addAll(mapper.apply(t));
+        }
+        return result;
+    }
 }
