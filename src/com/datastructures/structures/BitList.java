72c901741002bc697fd8da3b981616d95988a4a7
bhargav <bhargavreddypingle@gmail.com>
2025-02-05 14:07:00 -0600

    Bit List logic added

--- src/com/datastructures/structures/BitList.java
+++ src/com/datastructures/structures/BitList.java
@@ -1,4 +1,25 @@
 package com.datastructures.structures;
 
 public class BitList {
+    private int[] bits;
+    private int size = 0;
+
+    public BitList(int capacity) {
+        bits = new int[(capacity + 31) / 32];
+    }
+
+    public void add(boolean value) {
+        if (size >= bits.length * 32) throw new IndexOutOfBoundsException();
+        if (value) bits[size / 32] |= (1 << (size % 32));
+        size++;
+    }
+
+    public boolean get(int index) {
+        if (index >= size) throw new IndexOutOfBoundsException();
+        return (bits[index / 32] & (1 << (index % 32))) != 0;
+    }
+
+    public int size() {
+        return size;
+    }
 }
