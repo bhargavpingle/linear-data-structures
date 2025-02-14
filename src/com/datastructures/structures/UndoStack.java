f3af3de3345a298c3a4ca015c10eacbee8fa5a8f
bhargav <bhargavreddypingle@gmail.com>
2025-02-14 14:07:00 -0600

    undo stack undo stack

--- src/com/datastructures/structures/UndoStack.java
+++ src/com/datastructures/structures/UndoStack.java
@@ -1,4 +1,31 @@
 package com.datastructures.structures;
 
-public class UndoStack {
+import java.util.Stack;
+
+public class UndoStack<T> {
+    private final Stack<T> undo = new Stack<>();
+    private final Stack<T> redo = new Stack<>();
+
+    public void push(T state) {
+        undo.push(state);
+        redo.clear();
+    }
+
+    public T undo() {
+        if (undo.size() <= 1) return undo.peek();
+        T state = undo.pop();
+        redo.push(state);
+        return undo.peek();
+    }
+
+    public T redo() {
+        if (redo.isEmpty()) return undo.peek();
+        T state = redo.pop();
+        undo.push(state);
+        return state;
+    }
+
+    public T current() {
+        return undo.peek();
+    }
 }
