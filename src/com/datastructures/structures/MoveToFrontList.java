9b26bc9be0a902b89d203b3f8b3607b3e0f77dee
bhargav <bhargavreddypingle@gmail.com>
2025-03-02 14:07:00 -0600

    move to front list intialized

--- src/com/datastructures/structures/MoveToFrontList.java
+++ src/com/datastructures/structures/MoveToFrontList.java
@@ -1,4 +1,20 @@
 package com.datastructures.structures;
 
-public class MoveToFrontList {
+import java.util.LinkedList;
+
+public class MoveToFrontList<T> {
+    private final LinkedList<T> list = new LinkedList<>();
+
+    public void access(T item) {
+        list.remove(item);
+        list.addFirst(item);
+    }
+
+    public void add(T item) {
+        if (!list.contains(item)) list.addLast(item);
+    }
+
+    public LinkedList<T> getList() {
+        return list;
+    }
 }
