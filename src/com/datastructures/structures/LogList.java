dd12bbb34d87cb78add15cfd34042a6a021aa1b1
bhargav <bhargavreddypingle@gmail.com>
2025-03-10 14:07:00 -0500

    log list intialized

--- src/com/datastructures/structures/LogList.java
+++ src/com/datastructures/structures/LogList.java
@@ -1,4 +1,31 @@
 package com.datastructures.structures;
 
-public class LogList {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LogList<T> {
+    private final List<List<T>> versions = new ArrayList<>();
+
+    public LogList() {
+        versions.add(new ArrayList<>());
+    }
+
+    public void append(T value) {
+        List<T> newVersion = new ArrayList<>(current());
+        newVersion.add(value);
+        versions.add(newVersion);
+    }
+
+    public List<T> current() {
+        return versions.get(versions.size() - 1);
+    }
+
+    public List<T> getVersion(int index) {
+        return versions.get(index);
+    }
+
+    public int versionCount() {
+        return versions.size();
+    }
 }
