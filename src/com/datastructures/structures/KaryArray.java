95472d48f5996643f8ae7b9801adce6e1a232ba2
bhargav <bhargavreddypingle@gmail.com>
2025-03-16 14:07:00 -0500

    Kary array logic added

--- src/com/datastructures/structures/KaryArray.java
+++ src/com/datastructures/structures/KaryArray.java
@@ -1,4 +1,27 @@
 package com.datastructures.structures;
 
 public class KaryArray {
+    private final int[] data;
+    private final int k;
+
+    public KaryArray(int capacity, int k) {
+        this.data = new int[capacity];
+        this.k = k;
+    }
+
+    public int getParent(int index) {
+        return (index - 1) / k;
+    }
+
+    public int getChild(int index, int childIndex) {
+        return k * index + childIndex + 1;
+    }
+
+    public void set(int index, int value) {
+        data[index] = value;
+    }
+
+    public int get(int index) {
+        return data[index];
+    }
 }
