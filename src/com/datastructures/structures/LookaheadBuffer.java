1ff2426107a4ad9818f600bd40125db0e69b164b
bhargav <bhargavreddypingle@gmail.com>
2025-04-18 14:07:00 -0500

    looh ahead buffer logic added

--- src/com/datastructures/structures/LookaheadBuffer.java
+++ src/com/datastructures/structures/LookaheadBuffer.java
@@ -1,4 +1,27 @@
 package com.datastructures.structures;
 
-public class LookaheadBuffer {
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LookaheadBuffer<T> {
+    private final Queue<T> buffer = new LinkedList<>();
+    private final int maxSize;
+
+    public LookaheadBuffer(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public void add(T value) {
+        if (buffer.size() == maxSize) buffer.poll();
+        buffer.offer(value);
+    }
+
+    public T peekAhead(int k) {
+        if (k >= buffer.size()) return null;
+        return buffer.stream().skip(k).findFirst().orElse(null);
+    }
+
+    public int size() {
+        return buffer.size();
+    }
 }
