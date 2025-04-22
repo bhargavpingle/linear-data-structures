e91ecf2c56c9117f77997a1e87d85149e0289bb8
bhargav <bhargavreddypingle@gmail.com>
2025-04-22 14:07:00 -0500

    rotating array logic added

--- src/com/datastructures/structures/RotatingArray.java
+++ src/com/datastructures/structures/RotatingArray.java
@@ -1,4 +1,41 @@
 package com.datastructures.structures;
 
-public class RotatingArray {
+import java.util.Arrays;
+
+public class RotatingArray<T> {
+    private final T[] data;
+    private int offset = 0;
+
+    @SuppressWarnings("unchecked")
+    public RotatingArray(int capacity) {
+        data = (T[]) new Object[capacity];
+    }
+
+    public void set(int index, T value) {
+        data[(offset + index) % data.length] = value;
+    }
+
+    public T get(int index) {
+        return data[(offset + index) % data.length];
+    }
+
+    public void rotateRight(int times) {
+        offset = (offset - times + data.length) % data.length;
+    }
+
+    public void rotateLeft(int times) {
+        offset = (offset + times) % data.length;
+    }
+
+    public String toString() {
+        return Arrays.toString(toArray());
+    }
+
+    public T[] toArray() {
+        T[] result = Arrays.copyOf(data, data.length);
+        for (int i = 0; i < data.length; i++) {
+            result[i] = get(i);
+        }
+        return result;
+    }
 }
