56ad316f2ce9feb005766a648d73948edd61a626
bhargav <bhargavreddypingle@gmail.com>
2025-04-29 14:07:00 -0500

    jump table logic added

--- src/com/datastructures/structures/JumpTable.java
+++ src/com/datastructures/structures/JumpTable.java
@@ -1,4 +1,22 @@
 package com.datastructures.structures;
 
-public class JumpTable {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class JumpTable<T> {
+    private final Map<String, Consumer<T>> handlers = new HashMap<>();
+
+    public void register(String key, Consumer<T> handler) {
+        handlers.put(key, handler);
+    }
+
+    public void execute(String key, T input) {
+        Consumer<T> handler = handlers.get(key);
+        if (handler != null) {
+            handler.accept(input);
+        } else {
+            System.out.println("No handler for key: " + key);
+        }
+    }
 }
