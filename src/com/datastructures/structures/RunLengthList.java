11ab9c68ef9ca2b45a8e26266bc105257d45f087
bhargav <bhargavreddypingle@gmail.com>
2025-05-06 14:07:00 -0500

    run length list logic added

--- src/com/datastructures/structures/RunLengthList.java
+++ src/com/datastructures/structures/RunLengthList.java
@@ -1,4 +1,39 @@
 package com.datastructures.structures;
 
-public class RunLengthList {
+import java.util.ArrayList;
+import java.util.List;
+
+public class RunLengthList<T> {
+    private static class Pair<T> {
+        T value;
+        int count;
+
+        Pair(T value, int count) {
+            this.value = value;
+            this.count = count;
+        }
+
+        public String toString() {
+            return "(" + value + " x" + count + ")";
+        }
+    }
+
+    private final List<Pair<T>> data = new ArrayList<>();
+
+    public void add(T value) {
+        if (!data.isEmpty() && data.get(data.size() - 1).value.equals(value)) {
+            data.get(data.size() - 1).count++;
+        } else {
+            data.add(new Pair<>(value, 1));
+        }
+    }
+
+    public List<Pair<T>> getCompressed() {
+        return data;
+    }
+
+    public String toString() {
+        return data.toString();
+    }
 }
+
