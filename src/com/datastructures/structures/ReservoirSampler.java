649d0d43fc044ffdf87676e78ff23b8d085a924f
bhargav <bhargavreddypingle@gmail.com>
2025-05-10 14:07:00 -0500

    reservoir sampler logic added

--- src/com/datastructures/structures/ReservoirSampler.java
+++ src/com/datastructures/structures/ReservoirSampler.java
@@ -1,4 +1,20 @@
 package com.datastructures.structures;
 
-public class ReservoirSampler {
+import java.util.Random;
+
+public class ReservoirSampler<T> {
+    private T sample = null;
+    private int count = 0;
+    private final Random rand = new Random();
+
+    public void process(T item) {
+        count++;
+        if (rand.nextInt(count) == 0) {
+            sample = item;
+        }
+    }
+
+    public T getSample() {
+        return sample;
+    }
 }
