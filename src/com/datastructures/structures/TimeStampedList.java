920175519e4ce6e450bc817d4b59db668659392c
bhargav <bhargavreddypingle@gmail.com>
2025-05-12 14:27:00 -0500

    time stamped list logic added

--- src/com/datastructures/structures/TimeStampedList.java
+++ src/com/datastructures/structures/TimeStampedList.java
@@ -1,4 +1,30 @@
 package com.datastructures.structures;
 
-public class TimeStampedList {
+import java.time.Instant;
+import java.util.LinkedList;
+
+public class TimeStampedList<T> {
+    private static class Entry<T> {
+        final T data;
+        final Instant timestamp;
+
+        Entry(T data) {
+            this.data = data;
+            this.timestamp = Instant.now();
+        }
+
+        public String toString() {
+            return "[" + timestamp + "] " + data;
+        }
+    }
+
+    private final LinkedList<Entry<T>> entries = new LinkedList<>();
+
+    public void add(T item) {
+        entries.add(new Entry<>(item));
+    }
+
+    public LinkedList<Entry<T>> getAll() {
+        return entries;
+    }
 }
