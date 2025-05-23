a5c3918bd3114921d693f9bf0abfd71e195e1cbe
bhargav <bhargavreddypingle@gmail.com>
2025-05-23 14:27:00 -0500

    tagged union list logic added

--- src/com/datastructures/structures/TaggedUnionList.java
+++ src/com/datastructures/structures/TaggedUnionList.java
@@ -1,4 +1,41 @@
 package com.datastructures.structures;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaggedUnionList {
+    public static class Entry {
+        public enum Type { INT, STR, BOOL }
+
+        public final Type type;
+        public final Object value;
+
+        public Entry(Type type, Object value) {
+            this.type = type;
+            this.value = value;
+        }
+
+        public String toString() {
+            return type + ":" + value;
+        }
+    }
+
+    private final List<Entry> list = new ArrayList<>();
+
+    public void addInt(int i) {
+        list.add(new Entry(Entry.Type.INT, i));
+    }
+
+    public void addString(String s) {
+        list.add(new Entry(Entry.Type.STR, s));
+    }
+
+    public void addBool(boolean b) {
+        list.add(new Entry(Entry.Type.BOOL, b));
+    }
+
+    public List<Entry> getAll() {
+        return list;
+    }
 }
+
