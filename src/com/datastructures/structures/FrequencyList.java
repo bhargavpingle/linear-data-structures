fa3602d01de3f33460dafbea6059f60116ba3255
bhargav <bhargavreddypingle@gmail.com>
2025-05-30 14:27:00 -0500

    Frequency list logic added

--- src/com/datastructures/structures/FrequencyList.java
+++ src/com/datastructures/structures/FrequencyList.java
@@ -1,4 +1,20 @@
 package com.datastructures.structures;
 
-public class FrequencyList {
+import java.util.HashMap;
+import java.util.Map;
+
+public class FrequencyList<T> {
+    private final Map<T, Integer> freq = new HashMap<>();
+
+    public void add(T item) {
+        freq.put(item, freq.getOrDefault(item, 0) + 1);
+    }
+
+    public int getFrequency(T item) {
+        return freq.getOrDefault(item, 0);
+    }
+
+    public Map<T, Integer> getAll() {
+        return freq;
+    }
 }
