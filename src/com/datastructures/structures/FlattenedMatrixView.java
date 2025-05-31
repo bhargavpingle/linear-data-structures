bf5c82b59c60e41ba2a1d36e073c7f50e2ba6f8e
bhargav <bhargavreddypingle@gmail.com>
2025-05-31 14:27:00 -0500

    flattened matrix view logic added

--- src/com/datastructures/structures/FlattenedMatrixView.java
+++ src/com/datastructures/structures/FlattenedMatrixView.java
@@ -1,4 +1,24 @@
 package com.datastructures.structures;
 
 public class FlattenedMatrixView {
+    private final int[] data;
+    private final int rows, cols;
+
+    public FlattenedMatrixView(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.data = new int[rows * cols];
+    }
+
+    public void set(int row, int col, int val) {
+        data[row * cols + col] = val;
+    }
+
+    public int get(int row, int col) {
+        return data[row * cols + col];
+    }
+
+    public int[] rawData() {
+        return data;
+    }
 }
